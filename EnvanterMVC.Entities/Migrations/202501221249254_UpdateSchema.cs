c0a9e9c549e0d3426f360569f431254579e0de0b
Furkan Kizilay <Furkankizilay00@outlook.com>
2025-01-25 19:11:58 +0300

    Düzenlemeler

---
+++ EnvanterMVC.Entities/Migrations/202501221249254_UpdateSchema.cs
@@ -0,0 +1,18 @@
+﻿namespace EnvanterMVC.Entities.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateSchema : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.User", "IMEI", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.User", "IMEI", c => c.Int(nullable: false));
+        }
+    }
+}
